60a2323999037860cecce5500bc3a5684ecd943f
queens kisivuli <queenskisivuli@gmail.com>
2022-12-16 22:47:42 +0300

    height of a binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to measure the height of
+ *
+ * Return: height of the tree or 0 if tree is NULL
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    if (!tree)
+        return 0;
+
+    size_t left_height = binary_tree_height(tree->left);
+    size_t right_height = binary_tree_height(tree->right);
+
+    return 1 + (left_height > right_height ? left_height : right_height);
+}
