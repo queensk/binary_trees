a0e0811b97c720b6103efdb17503ed10bc0f8f9f
queens kisivuli <queenskisivuli@gmail.com>
2022-12-16 17:32:23 +0300

    fixed betty error

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,22 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * Recursively deletes a binary tree using post-order traversal.
- *
+ * binary_tree_delete - Recursively delete a binary tree
  * @tree: Pointer to the root node of the tree to delete
- *
- * The function first checks if the tree is NULL, in which case it returns
- * immediately. If the tree is not NULL, the function recursively deletes the
- * left and right subtrees of the tree using the binary_tree_delete() function
- * itself. Finally, it frees the memory occupied by the tree node by calling
- * free().
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (!tree)
 		return;
-
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
-    free(tree);
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
 }
