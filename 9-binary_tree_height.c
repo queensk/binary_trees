33cb0c5ea8045aa8100b7d48e09ecdcb3f391fb0
queens kisivuli <queenskisivuli@gmail.com>
2022-12-16 22:51:41 +0300

    fixed betty style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,10 +9,10 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
     if (!tree)
-        return 0;
+        return (0);
 
     size_t left_height = binary_tree_height(tree->left);
     size_t right_height = binary_tree_height(tree->right);
 
-    return 1 + (left_height > right_height ? left_height : right_height);
+    return (1 + (left_height > right_height ? left_height : right_height));
 }
