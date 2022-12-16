4a64bca789e8ff91b16f9b4043ec9298b42b7c85
queens kisivuli <queenskisivuli@gmail.com>
2022-12-17 00:02:33 +0300

    counts the leaves in a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -4,8 +4,7 @@
  * binary_tree_leaves - counts the leaves in a binary tree
  * @tree: tree to count the leaves from
  *
- * Return: number of leaves
- *         0 if tree is NULL
+ * Return: number of leaves or 0 if tree is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
