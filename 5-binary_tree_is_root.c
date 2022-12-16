92012f615b17a7684e0db2a62493ec7f60128812
queens kisivuli <queenskisivuli@gmail.com>
2022-12-16 19:16:01 +0300

    removed white space

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_root - checks if a node is a root.
  * @node: node to be checked.
- * 
+ *
  * Return: 1 if node is root else 0.
 */
 int binary_tree_is_root(const binary_tree_t *node)
