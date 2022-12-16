abdc0852682c8fd0e9ab770831b4e735fad5eb60
queens kisivuli <queenskisivuli@gmail.com>
2022-12-16 21:03:37 +0300

    fixed betty error

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_preorder - goes through a binary tree using pre-order traversal
  * @tree: tree to traverse
- * @func: pointer to a function to call for each node 
+ * @func: pointer to a function to call for each node
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
