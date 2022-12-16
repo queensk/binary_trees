3648ef9802e451958842b77a236a3372e9de27f0
queens kisivuli <queenskisivuli@gmail.com>
2022-12-16 22:54:33 +0300

    fixed error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,13 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
+	if (!tree)
+		return (0);
 
-    return (1 + (left_height > right_height ? left_height : right_height));
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
