b6d572b07d8d22ebb6548a51209f50547464b979
queens kisivuli <queenskisivuli@gmail.com>
2022-12-17 09:56:28 +0300

    binary tree fix

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,34 +9,55 @@
  * or NULL if no common ancestor was found
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                     const binary_tree_t *second)
+				     const binary_tree_t *second)
 {
-    size_t depth_first, depth_second;
+	size_t depth_first, depth_second;
 
-    if (!first || !second)
-        return (NULL);
+	if (!first || !second)
+		return (NULL);
 
-    for (depth_first = 0; first; depth_first++)
-        first = first->parent;
-    for (depth_second = 0; second; depth_second++)
-        second = second->parent;
+	depth_first = binary_tree_depth(first);
+	depth_second = binary_tree_depth(second);
 
-    while (depth_first > depth_second)
-    {
-        first = first->parent;
-        depth_first--;
-    }
-    while (depth_second > depth_first)
-    {
-        second = second->parent;
-        depth_second--;
-    }
+	while (depth_first > depth_second)
+	{
+		first = first->parent;
+		depth_first--;
+	}
+	while (depth_second > depth_first)
+	{
+		second = second->parent;
+		depth_second--;
+	}
+	while (first && second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+		first = first->parent;
+		second = second->parent;
+	}
+	return ((binary_tree_t *)first);
+}
 
-    while (first && second && first != second)
-    {
-        first = first->parent;
-        second = second->parent;
-    }
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: node to calculate the depth of
+ *
+ * Return: depth of the node
+ *         0 if tree is NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
 
-    return ((binary_tree_t *)first);
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
 }
