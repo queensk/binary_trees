ab1a45b0cf631c5b74145902eb3b13d7579030ed
queens kisivuli <queenskisivuli@gmail.com>
2022-12-17 13:20:34 +0300

    fixed error

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -7,59 +7,34 @@
  * Return: 1 if tree is avl
  *         0 otherwise
  */
-int binary_tree_is_avl(const binary_tree_t *tree)
+avl_t *avl_insert(avl_t **tree, int value)
 {
-	if (!tree)
-		return (0);
+    avl_t *new_node;
 
-	return (btia_helper(tree, INT_MIN, INT_MAX));
-}
+    if (!tree)
+        return (NULL);
 
-/**
- * btia_helper - helper that finds if a binary tree is an avl
- * @tree: pointer to the root node of the tree
- * @min: minimum value
- * @max: maximum value
- *
- * Return: 1 if tree is avl
- *         0 otherwise
- */
-int btia_helper(const binary_tree_t *tree, int min, int max)
-{
-	int path_l, path_r;
+    if (!*tree)
+    {
+        *tree = binary_tree_node(*tree, value);
+        return (*tree);
+    }
 
-	if (!tree)
-		return (1);
-	if (tree->n < min || tree->n > max)
-		return (0);
+    if (value < (*tree)->n)
+    {
+        new_node = avl_insert(&(*tree)->left, value);
+        (*tree)->left = new_node;
+    }
+    else if (value > (*tree)->n)
+    {
+        new_node = avl_insert(&(*tree)->right, value);
+        (*tree)->right = new_node;
+    }
+    else
+        return (NULL);
 
-	path_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	path_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+    (*tree)->height = 1 + MAX((*tree)->left ? (*tree)->left->height : 0, (*tree)->right ? (*tree)->right->height : 0);
+    *tree = binary_tree_balance(*tree);
 
-	if (abs(path_l - path_r) > 1)
-		return (0);
-
-	return (btia_helper(tree->left, min, tree->n - 1) &&
-		btia_helper(tree->right, tree->n + 1, max));
-	/* This is part of the BST check logic */
-}
-
-/**
- * binary_tree_height - measures the height of a binary tree
- * @tree: tree to measure the height of
- *
- * Return: height of the tree
- *         0 if tree is NULL
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t height_l = 0;
-	size_t height_r = 0;
-
-	if (!tree)
-		return (0);
-
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+    return (new_node);
 }
