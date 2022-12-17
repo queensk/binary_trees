f192422de302aa8adfdb0a007e3ae49ab294aea8
queens kisivuli <queenskisivuli@gmail.com>
2022-12-17 15:07:42 +0300

    t extracts the root node of a Max Binary Heap

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -9,5 +9,36 @@
  */
 int heap_extract(heap_t **root)
 {
+    heap_t *last_node, *temp;
+    int value;
 
+    if (!root || !*root)
+        return (0);
+
+    value = (*root)->n;
+
+    last_node = get_last_node(*root);
+    if (!last_node)
+        return (0);
+
+    if (last_node->parent)
+    {
+        if (last_node->parent->left == last_node)
+            last_node->parent->left = NULL;
+        else
+            last_node->parent->right = NULL;
+    }
+
+    if (last_node != *root)
+    {
+        (*root)->n = last_node->n;
+        temp = *root;
+        *root = last_node;
+        last_node = temp;
+    }
+
+    rebuild_heap(*root);
+
+    free(last_node);
+    return (value);
 }
