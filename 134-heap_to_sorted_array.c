336c6120d27b99bd3d0e639b67151d211f9b4e6b
queens kisivuli <queenskisivuli@gmail.com>
2022-12-17 15:02:26 +0300

    Binary Max Heap to a sorted array of integers

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -11,5 +11,19 @@
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
+    int *sorted_array;
+    size_t i;
 
+    if (!heap || !size)
+        return (NULL);
+
+    *size = heap_size(heap);
+    sorted_array = malloc(*size * sizeof(int));
+    if (!sorted_array)
+        return (NULL);
+
+    for (i = 0; i < *size; i++)
+        sorted_array[i] = heap_extract(&heap);
+
+    return (sorted_array);
 }
