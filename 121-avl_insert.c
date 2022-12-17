249a8c8a049474c66667d0e5d07b5e239141e2f7
queens kisivuli <queenskisivuli@gmail.com>
2022-12-17 14:29:25 +0300

    fixed error

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -7,27 +7,71 @@
  * Return: A pointer to the created node
  *         NULL on failure
  */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *tmp = *tree;
+	bst_t *second = NULL;
+	bst_t *new = binary_tree_node(NULL, value);
+
+	if (*tree == NULL)
+		*tree = new;
+
+	while (tmp)
+	{
+		second = tmp;
+		if (value < tmp->n)
+			tmp = tmp->left;
+		else if (value > tmp->n)
+			tmp = tmp->right;
+		else if (value == tmp->n)
+			return (NULL);
+	}
+
+	if (second == NULL)
+		second = new;
+	else if (value < second->n)
+	{
+		second->left = new;
+		new->parent = second;
+	}
+	else
+	{
+		second->right = new;
+		new->parent = second;
+	}
+
+	return (new);
+}
+
+/**
+ * avl_insert - inserts a value in an AVL Tree
+ * @tree: a double pointer to the root node of the AVL tree
+ * @value: value to insert
+ *
+ * Return: a pointer to the created node
+ *         NULL on failure
+ */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-    avl_t *new_node = NULL;
+	int balance = 0;
+	avl_t *node = bst_insert(tree, value);
 
-    if (!tree)
-        return (NULL);
+	balance = binary_tree_balance(*tree);
 
-    if (!*tree)
-    {
-        *tree = binary_tree_node(*tree, value);
-        return (*tree);
-    }
+	if (balance > 1 && value < node->left->n)
+		return (binary_tree_rotate_right(node));
+	if (balance < -1 && value > node->right->n)
+		return (binary_tree_rotate_left(node));
+	if (balance > 1 && value > node->left->n)
+	{
+		node->left = binary_tree_rotate_left(node->left);
+		return (binary_tree_rotate_right(node));
+	}
+	if (balance < -1 && value < node->right->n)
+	{
+		node->right = binary_tree_rotate_right(node->right);
+		return (binary_tree_rotate_left(node));
+	}
 
-    if (value < (*tree)->n)
-        new_node = avl_insert(&(*tree)->left, value);
-    else if (value > (*tree)->n)
-        new_node = avl_insert(&(*tree)->right, value);
-    else
-        return (NULL);
-
-    *tree = binary_tree_balance(*tree);
-
-    return (new_node);
+	return (node);
 }
